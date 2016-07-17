0123d16c4ec1960afefb668efb0ba4ae11c720da
Gabriel Bazán <gabrielbazan7@hotmail.com>
2016-07-17 14:28:19 -0300

    move profile info to side bar

--- gulpfile.js
+++ gulpfile.js
@@ -15,7 +15,7 @@ var paths = {
 gulp.task('default', ['sass', 'concat', 'concat-angular']);
 
 gulp.task('sass', function(done) {
-  gulp.src('./scss/**/*.scss')
+  gulp.src(['./scss/*.scss', './scss/partials/*.scss'])
     .pipe(sass())
     .on('error', sass.logError)
     .pipe(gulp.dest('./www/css/'))
