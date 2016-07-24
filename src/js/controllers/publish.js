878666f59a23f067c7e345afb57946a18877e755
Gabriel Bazán <gabrielbazan7@hotmail.com>
2016-07-24 16:47:28 -0300

    adding cordova photo plugins, adding general css and UI refactor

---
+++ src/js/controllers/publish.js
@@ -0,0 +1,46 @@
+'use strict';
+
+angular.module('starter.controllers').controller('publishController', function($scope, $cordovaImagePicker, $cordovaCamera) {
+
+  $scope.images = [];
+
+  $scope.fromCamera = function() {
+    var options = {
+      quality: 50,
+      destinationType: Camera.DestinationType.DATA_URL,
+      sourceType: Camera.PictureSourceType.CAMERA,
+      allowEdit: true,
+      encodingType: Camera.EncodingType.JPEG,
+      targetWidth: 100,
+      targetHeight: 100,
+      popoverOptions: CameraPopoverOptions,
+      saveToPhotoAlbum: false,
+      correctOrientation: true
+    };
+
+    $cordovaCamera.getPicture(options).then(function(imageData) {
+      var image = document.getElementById('myImage');
+      image.src = "data:image/jpeg;base64," + imageData;
+    }, function(err) {
+      // error
+    });
+  }
+
+  $scope.fromGallery = function() {
+    var options = {
+      maximumImagesCount: 10,
+      width: 800,
+      height: 800,
+      quality: 80
+    };
+
+    $cordovaImagePicker.getPictures(options)
+      .then(function(results) {
+        for (var i = 0; i < results.length; i++) {
+          console.log('Image URI: ' + results[i]);
+        }
+      }, function(error) {
+        // error getting photos
+      });
+  }
+});
